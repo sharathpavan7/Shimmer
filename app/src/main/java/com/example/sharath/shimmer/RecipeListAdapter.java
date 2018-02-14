1062684d992308a3d726cc5205d2ff5c3a2eda33
Venu Tadiparti <senchadev.v1@gmail.com>
2018-02-14 23:57:27 +0530

    New RecipeListAdapter class is added . This is the class that Recycler view uses . This class papulate the Recycler view.

---
+++ app/src/main/java/com/example/sharath/shimmer/RecipeListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.sharath.shimmer;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by YAKSEE on 14-02-2018.
+ */
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.MyViewHolder> {
+    private Context context;
+    private List<Recipe> cartList;
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_list_item,parent,false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        final Recipe recipe = cartList.get(position);
+        holder.name.setText(recipe.getName());
+        holder.chef.setText("By "+recipe.getChef());
+        holder.description.setText(recipe.getDescription());
+        holder.price.setText("Price: $"+recipe.getPrice());
+        holder.price.setText(recipe.getTimestamp());
+
+        Glide.with(context)
+                .load(recipe.getThumbnail())
+                .into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, description, price, chef, timestamp;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.name);
+            chef = view.findViewById(R.id.chef);
+            description = view.findViewById(R.id.description);
+            price = view.findViewById(R.id.price);
+            thumbnail = view.findViewById(R.id.thumbnail);
+            timestamp = view.findViewById(R.id.timestamp);
+        }
+    }
+}
