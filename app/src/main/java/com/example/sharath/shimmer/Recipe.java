2145370e2b4e0dddedfe37d5ff6222d1b3332fed
Venu Tadiparti <senchadev.v1@gmail.com>
2018-02-14 22:55:57 +0530

    New Recipe class is added . Representing recipe.

---
+++ app/src/main/java/com/example/sharath/shimmer/Recipe.java
@@ -0,0 +1,46 @@
+package com.example.sharath.shimmer;
+
+/**
+ * Created by Sharath on 14-02-2018.
+ */
+
+public class Recipe {
+    int id;
+    String name;
+    String description;
+    double price;
+    String thumbnail;
+    String chef;
+    String timestamp;
+
+    public Recipe() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getChef() {
+        return chef;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
